0206273d8076b28b1755bd2ba915817a78fc7919
Pavel Kizhlo <pavelkizh@gmail.com>
2023-04-04 14:08:25 +0300

    feat: add links for products to homepage

--- components/homepage/ProductsBlock.tsx
+++ components/homepage/ProductsBlock.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Card } from '@material-tailwind/react';
 import { openSans } from '@/utils/fonts';
 import { ProductGroup } from '@/types/product';
+import Link from 'next/link';
 
 interface ProductsBlockProps {
   products: ProductGroup[];
@@ -18,17 +19,19 @@ function ProductsBlock({ products }: ProductsBlockProps) {
       </h2>
       {products.map((group) => (
         <section key={group.id} className="flex flex-col items-center gap-6">
-          <h3 className="text-xl font-medium">{group.title.toUpperCase()}</h3>
+          <Link href={`/products/${group.slug}`}>
+            <h3 className="text-xl font-medium">{group.title.toUpperCase()}</h3>
+          </Link>
+
           <div className="flex flex-row flex-wrap justify-center gap-10">
             {group.items.map((item) => (
-              <Card
-                key={item.id}
-                className="relative h-[320px] w-[320px] cursor-pointer bg-gray-600"
-              >
-                <div className="absolute bottom-0 flex w-full items-center justify-center rounded-b-xl bg-blured py-4 text-center text-white">
-                  <h4 className="w-fit border-b-2 border-white">{item.title}</h4>
-                </div>
-              </Card>
+              <Link key={item.id} href={`/products/${group.slug}/${item.slug}`}>
+                <Card className="relative h-[320px] w-[320px] bg-gray-600">
+                  <div className="absolute bottom-0 flex w-full items-center justify-center rounded-b-xl bg-blured py-4 text-center text-white">
+                    <h4 className="w-fit border-b-2 border-white">{item.title}</h4>
+                  </div>
+                </Card>
+              </Link>
             ))}
           </div>
         </section>
