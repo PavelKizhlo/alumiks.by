c75020d027526db71b96b184e56f464915e7b98a
Pavel Kizhlo <pavelkizh@gmail.com>
2023-06-03 12:35:22 +0300

    feat: add adaptivity to workingProcessBlock

--- app/components/homepage/workingProcessBlock.tsx
+++ app/components/homepage/workingProcessBlock.tsx
@@ -12,17 +12,28 @@ export default async function WorkingProcessBlock() {
     <section className="bg-dark-shades py-10">
       <div className="container flex flex-col items-center gap-8 text-light-shades">
         <h2 className="heading-h2">Как мы работаем</h2>
-        <div className="grid relative grid-cols-6 after:block after:absolute after:w-8 after:h-8 after:content-['>'] after:text-warning text-[45px] leading-10 font-light after:top-[253px] after:-right-[15px]">
+        <div className="w-full md:w-3/4 lg:w-auto block lg:grid relative grid-cols-6 after:hidden lg:after:block after:absolute after:w-8 after:h-8 md:after:text-[30px] lg:after:text-[42px] after:content-['>'] after:text-warning text-[45px] leading-10 font-light lg:after:top-[163px] xl:after:top-[177px] 2xl:after:top-[189px] 3xl:after:top-[175px] lg:after:-right-[15px]">
           {stages.map((stage) => (
-            <div key={stage.id} className="group grid grid-rows-2">
-              <div className="flex justify-center items-center group-even:order-1 group-even:border-t-2 border-warning">
-                <div className="flex relative items-center justify-center w-1/2 aspect-square border-2 border-warning rounded-full after:block after:absolute after:w-0.5 after:h-36 after:bg-warning group-odd:after:top-full group-even:after:bottom-full">
-                  <Image width={68} height={68} src={stage.icon} alt="иконка" />
+            <div
+              key={stage.id}
+              className="group mb-16 last:mb-0 lg:mb-0 items-center lg:items-stretch justify-between flex lg:grid grid-rows-2"
+            >
+              <div className="flex lg:justify-center items-center group-even:order-2 lg:group-even:border-t-2 border-warning">
+                <div className="flex relative items-center justify-center w-[80px] sm1:w-[110px] sm:w-[140px] lg:w-1/2 aspect-square border-2 border-warning after:hidden rounded-full lg:after:block after:absolute after:w-0.5 after:h-36 after:bg-warning group-odd:after:top-full group-even:after:bottom-full">
+                  <Image
+                    width={68}
+                    height={68}
+                    src={stage.icon}
+                    className="w-2/3 lg:w-[54px] w-[68px]"
+                    alt="иконка"
+                  />
                 </div>
               </div>
-              <div className="text-center flex flex-col justify-start group-odd:border-t-2 border-warning group-odd:pt-20 group-even:pb-20">
-                <p className="heading-h4 mb-4">{stage.title}</p>
-                <p className="text-lg font-light">{stage.description}</p>
+              <span className="hidden sm1:block lg:hidden w-[40px] xs:w-[80px] md:w-[50px] group-even:order-1 h-[2px] bg-warning" />
+              <div className="lg:w-auto w-[260px] group-even:text-start group-odd:text-end lg:text-center justify-start lg:group-odd:border-t-2 border-warning lg:group-odd:pt-24 lg:group-even:pb-24 xl:group-odd:pt-28 xl:group-even:pb-28 3xl:group-odd:pt-32 3xl:group-even:pb-32">
+                <p className="heading-h4 text-lg sm1:text-xl lg:text-lg xl:text-xl 2xl:text-2xl">
+                  {stage.title}
+                </p>
               </div>
             </div>
           ))}
--- data/stages.ts
+++ data/stages.ts
@@ -3,41 +3,41 @@ import { Stage } from '@/types/stage';
 const STAGES: Stage[] = [
   {
     id: 1,
-    icon: '/icons/workSheme/sheme1.svg',
-    title: 'Уточнение требований',
+    icon: '/icons/workScheme/scheme1.svg',
+    title: 'Заявка на сайте или звонок в офис',
     description: 'Анализ требований заказчика и их уточнение.',
   },
   {
     id: 2,
-    icon: '/icons/workSheme/sheme2.svg',
-    title: 'Расчёт стоимости',
+    icon: '/icons/workScheme/scheme2.svg',
+    title: 'Консультация и уточнение деталей',
     description:
       'Рассчёт стоимости и минимальных сроков выполнения проекта. Вывод о реализуемости проекта в указаннные сроки',
   },
   {
     id: 3,
-    icon: '/icons/workSheme/sheme3.svg',
-    title: 'Предварительная вёрстка',
+    icon: '/icons/workScheme/scheme3.svg',
+    title: 'Расчет стоимости заказа',
     description: 'Предварительная вёрстка, конвертация в редактируемый формат',
   },
   {
     id: 4,
-    icon: '/icons/workSheme/sheme4.svg',
-    title: 'Определение тематики',
+    icon: '/icons/workScheme/scheme4.svg',
+    title: 'Оплата выбранным способом',
     description:
       'Определение тематики переводных файлов и требуемой квалификации исполнителей',
   },
   {
     id: 5,
-    icon: '/icons/workSheme/sheme5.svg',
-    title: 'Обобщение данных',
+    icon: '/icons/workScheme/scheme5.svg',
+    title: 'Изготовление заказа',
     description:
       'Обобщение и формализация данных о проекте(память переводов, график работы, требования заказчика, глоссарий, что-то ещё).',
   },
   {
     id: 6,
-    icon: '/icons/workSheme/sheme6.svg',
-    title: 'Назначение исполнителей',
+    icon: '/icons/workScheme/scheme6.svg',
+    title: 'Отгрузка либо доставка товара',
     description:
       'Назначение конкретных исполнителей(переводчиков, редактора) с учётом их специализации',
   },
